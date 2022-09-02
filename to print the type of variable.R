23417adeca20e1002dadcb3af2e796ea155d8a99
Suvadra Kundu <75944628+suvadra23@users.noreply.github.com>
2022-09-03 01:51:57 +0530

    Add files via upload

---
+++ to print the type of variable.R
@@ -0,0 +1,17 @@
+bool <- TRUE
+print(class(bool))
+bool2<- FALSE
+print(class(bool2))
+weight<- 63
+print(class(weight))
+height<- 182
+print(class(height))
+int_var<- 186L
+print(class(int_var))
+comp_val<- 3+2i
+print(class(comp_val))
+fruit<-"apple"
+print(class(fruit))
+raw_char<-rawToChar("Welcome to heaven")
+print(raw_char)
+print(class(raw_char))
